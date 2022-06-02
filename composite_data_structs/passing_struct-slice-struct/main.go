4f8b35978e47cc4c920ca54257ccab5660eed523
Jose Hernandez <josrhernandez1985@gmail.com>
2022-06-02 16:31:28 -0400

    added example when passing a struct with slices of struct

---
+++ composite_data_structs/passing_struct-slice-struct/main.go
@@ -0,0 +1,105 @@
+package main
+
+/*
+
+	to pass a composite type (ie a struct with a slice of struct): with variables
+		use:
+
+		you're passing the struct
+		...access the field, which is a slice of struct. so range that field
+		then access the correspoding field
+
+	<ul>
+    	{{range .Wisdom}}
+    	<li>{{.Name}} - {{.Motto}}</li>
+    	{{end}}
+	<ul>
+
+	<ul>
+    	{{range .Transport}}
+    	<li>{{.Manufacturer}} - {{.Model}} - {{.Doors}} door</li>
+    	{{end}}
+	</ul>
+
+*/
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type sage struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+type items struct {
+	Wisdom    []sage
+	Transport []car
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+
+	buddha := sage{
+		Name:  "Buddha",
+		Motto: "The Belief of no Beliefs",
+	}
+
+	gandhi := sage{
+		Name:  "Gandhi",
+		Motto: "Be the Change",
+	}
+
+	mlk := sage{
+		Name:  "Martin Luther King",
+		Motto: "Hatred never ceases with hatred but with love alone is healed",
+	}
+
+	jesus := sage{
+		Name:  "Jesus",
+		Motto: "Love all",
+	}
+
+	muhammad := sage{
+		Name:  "Muhammad",
+		Motto: "To overcome evil with good is good, to resist evil by evil is evil",
+	}
+
+	veh1 := car{
+		Manufacturer: "Toyota",
+		Model:        "Corolla",
+		Doors:        4,
+	}
+
+	veh2 := car{
+		Manufacturer: "Ford",
+		Model:        "F150",
+		Doors:        2,
+	}
+
+	sages := []sage{buddha, gandhi, mlk, jesus, muhammad}
+	cars := []car{veh1, veh2}
+
+	data := items{
+		Wisdom:    sages,
+		Transport: cars,
+	}
+
+	err := tpl.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
