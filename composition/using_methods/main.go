500c2a0d5ca0fbd4b600ed6c2ee30fc4d0d4acfd
Jose Hernandez <josrhernandez1985@gmail.com>
2022-06-06 15:15:13 -0400

    added example of using methods on a template

---
+++ composition/using_methods/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/template"
+)
+
+type person struct {
+	Name string
+	Age  int
+}
+
+func (p person) SomeProcessing() int {
+	return 7
+}
+
+func (p person) AgeDbl() int {
+	return p.Age * 2
+}
+
+func (p person) TakesArg(x int) int {
+	return x * 2
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+
+	p1 := person{
+		Name: "James Bond",
+		Age:  42,
+	}
+
+	err := tpl.Execute(os.Stdout, p1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println()
+}
