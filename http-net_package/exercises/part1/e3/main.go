a3203f5d92a1fa9715fe30c989ec2b9e45e5a433
Jose Hernandez <josrhernandez1985@gmail.com>
2022-06-11 15:27:59 -0400

    added comments to explain all the convertion going on

--- http-net_package/exercises/part1/e3/main.go
+++ http-net_package/exercises/part1/e3/main.go
@@ -56,6 +56,10 @@ func meHandler(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
+	// here I am converting the functions homePage, dogHandler, & meHandler to type http.HandlerFunc(ResponsiveWriter, *Request)
+	// type http.HandlerFunc(ResponsiveWriter, *Request) implements http.Handler interface since it has the method ServeHTTP(ResponsiveWriter, *Request)
+	// http.Handle functions takes a pattern (Type string, route!) and a http.Handler.
+	// since http.HandlerFunc is also type http.Handler the below code works!
 	http.Handle("/", http.HandlerFunc(homePage))
 	http.Handle("/dog/", http.HandlerFunc(dogHandler))
 	http.Handle("/me/", http.HandlerFunc(meHandler))
