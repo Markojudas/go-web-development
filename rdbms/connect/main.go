eb3ebcf02af0d2ba49dcceede5aa770b7f7ba668
Jose Hernandez <josrhernandez1985@gmail.com>
2022-06-21 14:14:02 -0400

    interacted with AWS RDS through mysql cli & Go! pending: better docs

--- rdbms/connect/main.go
+++ rdbms/connect/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"io"
+	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,8 +12,25 @@ import (
 var db *sql.DB
 var err error
 
+func check(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
 func main() {
-	db, err = sql.Open("mysql", "markojudas:Anewfire=123123!!@tcp(database-1.c5tkerwyxnm3.us-east-1.rds.amazonaws.com:3306)")
+	db, err = sql.Open("mysql", "marko:Anewfire=123123!!@tcp(godb.c5tkerwyxnm3.us-east-1.rds.amazonaws.com:3306)/test?charset=utf8")
+	check(err)
+	defer db.Close()
+
+	err = db.Ping()
+	check(err)
+
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	err := http.ListenAndServe(":8080", nil)
+	check(err)
+
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
